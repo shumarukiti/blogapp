fb0bd9ac3ebdcce85166fa47eb7477ded69b6959
Shujiro Kanaya <j1info.com@gmail.com>
2020-12-27 12:13:27 +0900

    aaa

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,8 +9,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 10.times do
-Article.create(
+  Article.create(
     title: Faker::Lorem.sentence(word_count: 5),
     content: Faker::Lorem.sentence(word_count: 100)
-)
+  )
 end
